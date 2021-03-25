8c50da63aea69a642529bd4f7fb5c3174aac015f
RyanFi <ryanmcmahon94@gmail.com>
2021-03-25 19:21:03 -0400

    Complete

---
+++ index.js
@@ -0,0 +1,12 @@
+//make function expression divide
+//divide 2000 by 100
+//add arrow function called square, square x
+//add arrow function called add, add a+b
+
+const divide = function() {
+  return 2000 / 100;
+}
+
+const square = x => x * x;
+
+const add = (a, b) => a + b;
